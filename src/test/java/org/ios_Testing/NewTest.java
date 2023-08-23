64d41ea69c2b5d80f5ebc386bc812d3c90eecfa9
ShashikantKokitakar <Shashikant.kokitakar@facillima.com>
2023-08-23 10:35:03 +0530

    ios testing

---
+++ src/test/java/org/ios_Testing/NewTest.java
@@ -0,0 +1,33 @@
+package org.ios_Testing;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class NewTest {
+  @Test
+  public void f() throws MalformedURLException 
+  {
+	  DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+      capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.5"); // Replace with your iOS version
+      capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 12 Pro"); // Replace with your device name
+      capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+      
+      URL url=new URL("http://localhost:4723/wd/hub");
+
+      IOSDriver driver = new IOSDriver(url, capabilities);
+
+     driver.get("https://www.amazon.in/");
+     
+     String s1=driver.getTitle();
+     System.out.println(s1);
+
+      driver.quit();
+  }
+}
