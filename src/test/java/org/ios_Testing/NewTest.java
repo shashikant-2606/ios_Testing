7dd939395f8ff2e1db0dd2d2d5c05b2c8507cc2b
ShashikantKokitakar <Shashikant.kokitakar@facillima.com>
2023-08-24 17:53:37 +0530

    message

--- src/test/java/org/ios_Testing/NewTest.java
+++ src/test/java/org/ios_Testing/NewTest.java
@@ -9,20 +9,21 @@ import java.net.URL;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
 
 public class NewTest {
   @Test
   public void f() throws MalformedURLException 
   {
 	  DesiredCapabilities capabilities = new DesiredCapabilities();
-      capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-      capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "16.2"); // Replace with your iOS version
-      capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11"); // Replace with your device name
+      capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS);
+      //capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "16.2"); // Replace with your iOS version
+     // capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11"); // Replace with your device name
       capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
       capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
       //capabilities.setCapability("webDriverAgentUrl", WDAServer.SERVER_URL);
       
-      URL url=new URL("http://0.0.0.0:4723/");
+      URL url=new URL("http://127.0.0.1:4723/wd/hub/");
 
       IOSDriver driver = new IOSDriver(url, capabilities);
 
